ac553b023f6a4e759439b939a52e71dbaaa58d57
Mark Milligan <mtm20176@gmail.com>
2021-01-05 13:58:14 -0600

    install script and git folder

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Hello install.sh"
 
-echo "Clon-ing dotfiles..."
+echo "Clon-ing / link-ing dotfiles ..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install HOME-based bash files
