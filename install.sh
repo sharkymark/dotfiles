5ee36e3503fbc910881b50eec8e0a5899c667d29
Mark Milligan <mtm20176@gmail.com>
2021-01-06 16:31:03 -0600

    redo git dotfiles

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ echo "install.sh"
 echo "Copying dotfiles ..."
 
 echo ".gitconfig and .gitignore_global"
-cp -r ./dotfiles/git/.gitconfig ./dotfiles/git/.gitignore_global ~
+cp -r ~/dotfiles/git/.gitconfig ~/dotfiles/git/.gitignore_global ~
