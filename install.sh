0441807671c9f9a12bd2340483a457c818d014fb
Mark Milligan MBP <mtm20176@gmail.com>
2021-04-02 07:06:58 -0500

    add fish install

--- install.sh
+++ install.sh
@@ -6,3 +6,16 @@ echo "Copying dotfiles ..."
 
 echo ".gitconfig and .gitignore_global"
 cp -r ~/dotfiles/git/.gitconfig ~/dotfiles/git/.gitignore_global ~
+
+echo "install fish shell"
+
+FISH_BINARY=/usr/bin/fish
+FISH_PATH=/usr/bin
+
+if [ ! -f $FISH_BINARY ] ; then
+    sudo apt-get update
+    sudo apt-get install -y fish
+    echo "installing fish in $FISH_PATH"
+else
+    echo "fish already installed"
+fi
