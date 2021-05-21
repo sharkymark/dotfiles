5ddd65242dfdc9bd662b0e4bf8bea52cf36e5dfb
Mark Milligan <mtm20176@gmail.com>
2021-05-21 16:35:26 -0500

    settings.json copying

--- install.sh
+++ install.sh
@@ -4,42 +4,44 @@ echo "install.sh"
 
 echo "Copying dotfiles ..."
 
-echo ".gitconfig and .gitignore_global"
+echo "STEP: .gitconfig and .gitignore_global"
 cp -r ~/dotfiles/git/.gitconfig ~/dotfiles/git/.gitignore_global ~
 
-echo "copy config.json to code-server directory"
+echo "STEP: copy config.json to code-server directory"
 PATH_CS_1="$HOME/.local/share/code-server/User"
-PATH_VS_1=$HOME/Library/Application\ Support/Code/User
-COMMAND_1="cp $HOME/dotfiles/Code/User/settings.json ."
+PATH_VS_1="$HOME/Library/Application Support/Code/User"
+COMMAND="cp ./Code/User/settings.json"
 
 if [ -d $PATH_CS_1 ]; then
-    echo 'code-server VS Code settings.json already exists'
-    cd $PATH_CS_1
-    $COMMAND_1
+    echo 'code-server folder exists, copying settings.json'
+    $COMMAND $PATH_CS_1
 fi
 
-if [ -d $PATH_VS_1 ]; then
-    echo 'VS Code settings.json already exists'
-    cd $PATH_VS_1
-    $COMMAND_1 
+if [ -d "$PATH_VS_1" ]; then
+    echo 'VS Code exists, copying settings.json'
+    $COMMAND "$PATH_VS_1"
 fi
 
 
 
-echo "install fish shell"
+echo "STEP: install fish shell"
 
 FISH_BINARY=/usr/bin/fish
 FISH_PATH=/usr/bin
 
-if [ ! -f $FISH_BINARY ] ; then
-    sudo apt-get update
-    sudo apt-get install -y fish
-    echo "installing fish in $FISH_PATH"
-else
-    echo "fish already installed"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [ ! -f $FISH_BINARY ] ; then
+        sudo apt-get update
+        sudo apt-get install -y fish
+        echo "installing fish in $FISH_PATH"
+    else
+        echo "fish already installed"
+    fi
 fi
 
 
 # locations for VS Code settings.json
 # Mac $HOME/Library/Application Support/Code/User/
 # Linux $HOME/.config/Code/User/
+# locations for code-server settings.json
+# $HOME/.local/share/code-server/User
