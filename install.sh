a2e1f4712d8f823202d769c356b60a2c2bee85d2
Mark Milligan <mtm20176@gmail.com>
2023-04-23 17:20:28 +0000

    chore: zshrc

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-echo "install.sh"
+echo "dotfiles repo install.sh"
 
 echo "Copying dotfiles ..."
 
-echo "STEP: .gitconfig and .gitignore_global"
+echo "STEP: copying .gitconfig and .gitignore_global"
 cp -r ./git/.gitconfig ./git/.gitignore_global ~
 
-echo "STEP: copy config.json to code-server directory"
-PATH_APPS="/coder/apps"
+
 PATH_CS_1="$HOME/.local/share/code-server"
 PATH_CS_2="$HOME/.local/share/code-server/User"
 PATH_VS_1="$HOME/Library/Application Support/Code/User"
@@ -16,34 +15,38 @@ COMMAND_S="cp ./Code/User/settings.json"
 COMMAND_K="cp ./Code/User/keybindings.json"
 COMMAND_T="cp ./Code/User/tasks.json"
 
-cp ./coder/.bashrc $HOME
+echo "STEP: copying shell dotfiles e.g., .bashrc, .zshrc"
+
+if [ "$SHELL" == "/bin/bash" ]; then
+  cp /shell/bash/.bashrc $HOME/.bashrc
+elif [ "$SHELL" == "/bin/zsh" ]; then
+  cp /shell/zsh/.zshrc $HOME/.zshrc
+elif [ "$SHELL" == "/usr/local/fish" ]; then
+  cp /shell/fish/config.fish $HOME/.config/fish/config.fish
+else
+  echo "no unix shell dotfiles copied"
+fi
+
+echo "STEP: copying VS Code-related config files"
 
 if [ -d $PATH_CS_1 ]; then
 
-    echo 'code-server folder exists, copying settings.json and keybindings.json' 
-    if [ -d $PATH_CS_2 ]; then
-        echo "User directory found"              
+    echo 'code-server directory exists (i.e., a Coder remote workspace), copying settings.json, keybindings.json, tasks.json' 
+    if [ -d $PATH_CS_2 ]; then          
     else
-        echo "User directory not found, make directory"
         mkdir "$PATH_CS_1"/User    
     fi
 
     # copy settings, keybindings and tasks.json
     $COMMAND_S $PATH_CS_2
-    echo "settings.json copied"
     $COMMAND_K $PATH_CS_2
-    echo "keybindings.json copied"
     $COMMAND_T $PATH_CS_2
-    echo "tasks.json copied"
 
 fi
 
 if [ -d "$PATH_VS_1" ]; then
-    echo 'VS Code exists, copying settings.json and keybindings.json'
+    echo 'locally-installed VS Code found, copying settings.json, keybindings.json, tasks.json'
     $COMMAND_S "$PATH_VS_1"
-    echo "settings.json copied"
     $COMMAND_K "$PATH_VS_1"
-    echo "keybindings.json copied"
-    $COMMAND_T "$PATH_VS_1"
-    echo "tasks.json copied"    
+    $COMMAND_T "$PATH_VS_1"  
 fi
---
+++ shell/zsh/.zshrc
@@ -0,0 +1,26 @@
+
+export EDITOR='code -w'
+
+# docker socket symlink if it is missing - needed for docker desktop to build images and create and start containers
+if [ ! -L '/var/run/docker.sock' ]; then
+    sudo ln -s "$HOME/.docker/run/docker.sock" /var/run/docker.sock;
+fi
+
+# for debugging later
+#ls -lah /var/run/docker.sock
+#ls -lah ~/.docker/run/docker.sock
+
+# zsh configurations
+export HISTFILE="$HOME/.zsh_history"
+export HISTSIZE=999999
+export HISTFILESIZE=999999
+export SAVEHIST=$HISTSIZE
+# initialize completion
+autoload -U compinit; compinit
+_comp_options+=(globdots) # With hidden files
+#prompt
+#PS1="%n@%m %1~ %# "
+PS1="🦈 %1~ %# "
+
+# ruby environment manager rbenv initialization
+eval "$(rbenv init -)"
