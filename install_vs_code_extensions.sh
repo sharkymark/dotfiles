4749e7cce77ee682986d64f229b31f5b3a52dfc3
Mark Milligan <mtm20176@gmail.com>
2024-03-22 22:09:28 +0000

    chore: vscode extension list for cs and vsc

--- install_vs_code_extensions.sh
+++ install_vs_code_extensions.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Path to your list of extensions
-EXTENSIONS_FILE="vs_code_extensions_list.txt"
+if [ -d $PATH_CS_1 ]; then
+    EXTENSIONS_FILE="code-server_extensions_list.txt"
+elif [ -d "$PATH_VS_1" ]; then
+    EXTENSIONS_FILE="vs_code_extensions_list.txt"
+else
+    echo "⛔️ no IDE installed"
+    exit 1
+fi
 
 # Read each line in the extensions file and install the extension
 while IFS= read -r extension
