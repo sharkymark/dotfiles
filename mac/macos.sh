42b90a038024d747dc27411c340f7d4a96da8529
Mark Milligan <mtm20176@gmail.com>
2025-04-22 07:42:05 -0500

    chore: remove black background settings from macOS defaults script

--- mac/macos.sh
+++ mac/macos.sh
@@ -9,10 +9,6 @@ killall Finder 2> /dev/null # Ignore error if Finder isn't running
 # Mouse tracking speed that I like
 defaults write -g com.apple.mouse.scaling -float 2.5
 
-# Set black background on desktop
-defaults write com.apple.desktop BackgroundType -int 1
-defaults write com.apple.desktop SolidColor -array 0 0 0
-
 killall Dock 
 
 echo "macOS defaults applied."
